924a980754c82c6978bade47584940c914ac5333
Ashi Gupta <118842314+Ashi12218604@users.noreply.github.com>
2025-07-13 09:40:42 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -104,7 +104,7 @@ const Hero = () => {
           >
             <div className="relative h-64 w-64 md:h-80 md:w-80 rounded-full overflow-hidden border-4 border-purple-400 shadow-xl hover:shadow-purple-300 transition-shadow duration-300">
               <img
-                src="profile1.jpg"
+                src="pp.jpg"
                 alt="Ashi Gupta"
                 className="object-cover w-full h-full hover:scale-105 transition-transform duration-300"
               />
